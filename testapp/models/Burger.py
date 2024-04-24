283b955f5f5a1c0984a3462563ffb73e042131fa
qma <mk348437@gmail.com>
2024-04-24 16:28:55 +0200

    zaj 4

--- testapp/models/Burger.py
+++ testapp/models/Burger.py
@@ -2,23 +2,49 @@ from django.db import models
 from datetime import datetime
 from datetime import timedelta
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+import uuid
 
 
 class BurgerModel(models.Model):
+    class Meat(models.TextChoices):
+        CHICKEN = "CH", _("Chicken")
+        VEGE = "VG", _("Vegetarian")
+        STEAK = "ST", _("Steak")
+        FISH = "FI", _("Fish")
+
     name = models.CharField(max_length=255)
     price = models.PositiveSmallIntegerField(default=1)
     weight = models.PositiveSmallIntegerField(default=100)
-    meat = models.TextChoices("Meat", "CHIKCKEN VEGE STEAK FISH")
+    meat = models.CharField("Meat", max_length=2, choices=Meat, default=Meat.CHICKEN)
     bacon = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    pk = models.IntegerField(name="unique id")
+    pk = models.UUIDField(name="unique id", default=uuid.uuid4())
     eaten = False
 
     def __str__(self):
         return self.name
 
     def is_fresh(self):
-        return (datetime.now().date() - self.created) <= timedelta(minutes=5)
+        return self.time_since_created() <= timedelta(minutes=5)
+
+    def is_eaten(self):
+        return self.eaten
+
+    def is_vegan(self):
+        return self.meat == self.Meat.VEGE and not self.bacon
+
+    def is_large(self):
+        return self.weight > 200
+
+    def is_expensive(self):
+        return self.price > 25
+
+    def is_named(self):
+        return not (self.name == None or self.name == "")
+
+    def time_since_created(self):
+        return datetime.now().astimezone() - self.created
 
     def get_absolute_url(self):
         return reverse('burger-detail', kwargs={'pk': self.pk})
--- testapp/tests.py
+++ testapp/tests.py
@@ -6,10 +6,51 @@ from datetime import datetime, timedelta
 # Create your tests here.
 class BurgerModelTest(TestCase):
 
-    def setUp(self) -> None:
-        Burger.objects.create(name="Test burger", price=100, weight=250, meat="Fish", bacon=False, created=(datetime.now() - timedelta(minutes=6)), pk=-1)
+    def setUp(self):
+        Burger.objects.create(name="Test burger", price=100, weight=250, meat=Burger.Meat.VEGE, bacon=False, created=(datetime.now() - timedelta(minutes=4)))
+
+    def test_values(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.price == 100)
+        self.assertTrue(burger.weight == 250)
+        self.assertTrue(burger.meat == Burger.Meat.VEGE)
+        self.assertFalse(burger.bacon)
+
 
     def test_is_fresh(self):
         burger = Burger.objects.get(name="Test burger")
 
-        self.assertTrue(not burger.is_fresh())
+        self.assertTrue(burger.is_fresh())
+
+    def test_is_eaten(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertFalse(burger.is_eaten())
+        burger.eat()
+        self.assertTrue(burger.is_eaten())
+
+    def test_is_vegan(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.is_vegan())
+
+    def test_is_large(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.is_large())
+
+    def test_is_expensive(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.is_expensive())
+
+    def test_is_named(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.is_named())
+
+    def test_creation_time(self):
+        burger = Burger.objects.get(name="Test burger")
+
+        self.assertTrue(burger.time_since_created() - timedelta(minutes=4) < timedelta(seconds=1))
